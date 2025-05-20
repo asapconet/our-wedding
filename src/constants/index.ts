60f07b3f3af80aca507ac1f843bb3c29689847fb
ASAP_A1 <aaronrickymeek@gmail.com>
2025-05-20 23:57:06 +0100

    feat: upload real time gallery image prev

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,6 +1,4 @@
 import type { ItineraryItem } from "../types/index";
-import DesertShot from "../assets/images/desertPhoto.png";
-
 interface DailySchedule {
   [key: string]: ItineraryItem[];
 }
@@ -72,33 +70,33 @@ interface Image {
 
 export const galleryImages: Image[] = [
   {
-    src: DesertShot,
-    alt: "Desert meeting",
-    caption: "Team gathering in the desert",
+    src: "https://res.cloudinary.com/dsz3obfpx/image/upload/v1747779227/nativeOne_prxq80.jpg",
+    alt: "Asoke one",
+    caption: "Asokeing",
   },
   {
-    src: DesertShot,
-    alt: "Company retreat",
-    caption: "Annual company retreat",
+    src: "https://res.cloudinary.com/dsz3obfpx/image/upload/v1747781165/englishShirtOne_u7zwku.jpg",
+    alt: "Englishha",
+    caption: "Asokeinging",
   },
   {
-    src: DesertShot,
-    alt: "Team building",
-    caption: "Team building activity",
+    src: "https://res.cloudinary.com/dsz3obfpx/image/upload/v1747781569/bride_zb0jji.jpg",
+    alt: "Blide",
+    caption: "Briding",
   },
   {
-    src: DesertShot,
-    alt: "Office space",
-    caption: "Our new office space",
+    src: "https://res.cloudinary.com/dsz3obfpx/image/upload/v1747779679/englishTwo_buatws.jpg",
+    alt: "Englishing two",
+    caption: "Englishinging",
   },
   {
-    src: DesertShot,
-    alt: "Product launch",
-    caption: "Latest product launch event",
+    src: "https://res.cloudinary.com/dsz3obfpx/image/upload/v1747781380/groom_bgfb4r.jpg",
+    alt: "groom one",
+    caption: "Grooming",
   },
   {
-    src: DesertShot,
-    alt: "Community outreach",
-    caption: "Community outreach program",
+    src: "https://res.cloudinary.com/dsz3obfpx/image/upload/v1747779715/best_3_owuag4.jpg",
+    alt: "Naitive four",
+    caption: "Nativitity",
   },
 ];
